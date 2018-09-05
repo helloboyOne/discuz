9792a67b09ad242b7b290841a8b3043ceb1a72d4
yang weiwei <yangww7@lenovo.com>
2018-09-05 18:22:27 +0800

    更换数据库

--- upload/data/log/201809_illegallog.php
+++ upload/data/log/201809_illegallog.php
@@ -1,2 +1,4 @@
 <?PHP exit;?>	1536131619	admin	a***n	Ques #0	60.247.104.81
 <?PHP exit;?>	1536131643	admin	12***6	Ques #0	60.247.104.81
+<?PHP exit;?>	1536139057	admin	11***1	Ques #0	::1
+<?PHP exit;?>	1536139069	admin	11***1	Ques #0	::1
